4d5a71ff7643341d3a07928ff2e6a64ce7a8464d
root <root@x.massivesoft.net>
2017-10-16 21:23:34 +0200

    updated installer

--- template/install.sh
+++ template/install.sh
@@ -1,5 +1,8 @@
 # assassins installation script
 
-mysql -e 'CREATE DATABASE assassins;'
-mysql -e 'GRANT SELECT,UPDATE,INSERT,DELETE PRIVILEGES ON `assassins` . * TO "assassins_web_user"@"localhost" IDENTIFIED BY "HDR3Z22c6bWyNZwJdkeCPAWL";'
+CONFIG_FILE="config.php";
 
+DATABASE_PASS=`sed -n '/DATABASE_PASS/p' "${CONFIG_FILE}" | awk -F',' '{print $2}' | cut -d '"' -f2`
+
+mysql -e 'CREATE DATABASE assassins;'
+mysql -e 'GRANT SELECT,UPDATE,INSERT,DELETE PRIVILEGES ON `assassins` . * TO "assassins_web_user"@"localhost" IDENTIFIED BY "${DATABASE_PASS}";'
