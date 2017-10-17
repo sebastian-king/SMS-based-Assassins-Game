a39a66eb52be5061486511d1961500ca6d0679a7
root <root@x.massivesoft.net>
2017-10-17 21:37:09 +0200

    separated generic and specialist functions

--- template/top.php
+++ template/top.php
@@ -2,7 +2,8 @@
 
 require_once("config.php");
 require_once(BASE_PATH . "/api/twilio/Twilio/autoload.php");
-require_once(BASE_PATH . "/template/functions.php");
+require_once(BASE_PATH . "/template/functions/assassins.php");
+require_once(BASE_PATH . "/template/functions/system.php");
 
 $db = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASS, DATABASE_NAME)
 	or die("Unable to connect to database");
