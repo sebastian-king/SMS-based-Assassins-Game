5b8c0077d59d58e422c08143377c1504d9c97178
root <root@x.massivesoft.net>
2017-10-27 00:17:48 +0200

    removed unused images, improved usability of shuffle script

--- admin/shuffle.php
+++ admin/shuffle.php
@@ -1,6 +1,13 @@
 <?php
-set_time_limit(30);
+
+if (php_sapi_name() !== 'cli') {
+        die("This is a CLI only script, please run it on the server's terminal.");
+}
+
+set_time_limit(300); // WARNING: This script can take a long time to run, it will time out after 5 minutes simply because this can stop the server crashing if somehow the recursion ends in an infinite loop
+
 require("../template/top.php");
+
 //
 if (preg_match("/sandbox/i", @$argv[1])) {
 	$SANDBOX = true;
@@ -9,9 +16,9 @@ if (preg_match("/sandbox/i", @$argv[1])) {
 }
 //
 
-echo "<pre>\n".exec("date")."\n";
+echo "<pre>", PHP_EOL, date('l jS \of F Y h:i:s A'), PHP_EOL;
 $q = $db->query("SELECT * FROM players WHERE target > ''") or die($db->error);
-echo $q->num_rows . "\n";
+echo "Total number of players to be shuffled: ", $q->num_rows, PHP_EOL;
 
 $players = array();
 $shuffled_players = array();
@@ -38,9 +45,9 @@ $players[key($players)]['target'] = $tmp;
 
 while (count($players) >= 1) {
 	$player = current(array_keys($players));
-	echo "Starting with player: ".$player."\n";
-	echo "Ending with player: ".pool($player)."\n";
-	echo "Players remaining: ".count($players)."\n------\n";
+	echo "Starting with player: ", $player, PHP_EOL;
+	echo "Ending with player: ", pool($player), PHP_EOL;
+	echo "Players remaining: ", count($players), PHP_EOL, "------", PHP_EOL;
 }
 
 function pool($id) {
@@ -55,32 +62,32 @@ function pool($id) {
 	}
 }
 
-echo var_export($players, true),"\n";
+echo "No. of players that haven't been shuffled in the pool: ", count($players) , PHP_EOL;
 
 if ($SANDBOX) {
-	die("\nSANDBOX\n");
+	die(PHP_EOL . "The sandbox parameter was specified, therefore we are exiting now without writing changes to the database and without texting anyone." . PHP_EOL);
 }
 
-echo "\nKILL NOW IF POOL LARGER THAN 1\n";
+echo PHP_EOL, "If the number above is larger than 0, kill this program NOW.", PHP_EOL;
 
 $i = 0;
 while ($i < 10) {
-	echo "$i		\r";
+	echo "Seconds before the shuffle goes live: $i	\r";
 	$i++;
 	sleep(1);
 }
 
 foreach ($shuffled_players as $key => $val) {
-	//echo "($key) {$val['name']} => {$shuffled_players[$shuffled_players[$key]['target']]['name']} ({$shuffled_players[$key]['target']})\n";
+	echo "(ID:$key) NAME:{$val['name']} => TARGET:{$shuffled_players[$shuffled_players[$key]['target']]['name']} TARGET_ID:({$shuffled_players[$key]['target']})", PHP_EOL;
 	$db->query("UPDATE players SET target = '".$db->real_escape_string($shuffled_players[$key]['target'])."' WHERE id = '$key' LIMIT 1") or die($db->error);
 }
 
 // shuffle complete & writen to db
-// now start sending
+// now start sending texts with people's new targets
 
 $q = $db->query("SELECT * FROM players WHERE target > '' ORDER BY id ASC");
 while ($r = $q->fetch_array(MYSQLI_ASSOC)) {
-		echo "(".str_pad($r['validated'], 5).") " . str_pad($r['name'], 25, " ") . " => 0 (".$r['pin'].") => ".str_pad(uid2name($r['target']), 25, " ") . PHP_EOL;
+		echo "VALIDATED:(", str_pad($r['validated'], 5), ") NAME:", str_pad($r['name'], 25, " "), " PIN:(", $r['pin'], ") TARGET:", str_pad(uid2name($r['target']), 25, " "), PHP_EOL;
 		try {
 				$sms = $twilio_client->messages->create(
 						$r['phone'],
@@ -90,8 +97,8 @@ while ($r = $q->fetch_array(MYSQLI_ASSOC)) {
 						)
 				);
 		} catch (Twilio\Exceptions\RestException $e) {
-			echo var_dump("ERROR CAUGHT $e")."\n";
+			echo var_dump("ERROR CAUGHT $e") . PHP_EOL;
 		}
-		//usleep(100000);
+		//usleep(100000); // this might be helpful if Twilio enforces a rate limit
 }
 echo "DONE";
